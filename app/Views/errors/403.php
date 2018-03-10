f3ebf9b6bf0b3307800eaf82260a965bf4068bcf
maximehgt <maximehugonnet26@gmail.com>
2018-03-10 12:07:52 +0100

    Ajout des pags erreurs pour insertions

---
+++ app/Views/errors/403.php
@@ -0,0 +1,2 @@
+<p>Erreur 403 - L'accès à cette page vous est interdit</p>
+<a href="http://localhost/jean-forteroche/public/index.php" class="btn btn-warning">Retour à l'accueil</a>
---
+++ app/Views/errors/404.php
@@ -0,0 +1,2 @@
+<p>Erreur 404 - Page introuvable</p>
+<a href="http://localhost/jean-forteroche/public/index.php" class="btn btn-warning">Retour à l'accueil</a>
